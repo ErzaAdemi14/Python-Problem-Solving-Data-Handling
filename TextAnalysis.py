33fedde31f9750a5a2b6e5b47437a1422b5aba8f
Erza Ademi <ademierza14@gmail.com>
2026-03-25 16:34:16 +0100

    Completed Task 4 - Text Analysis

---
+++ TextAnalysis.py
@@ -0,0 +1,12 @@
+sentence = "Python is great and python is easy to learn"
+
+words = sentence.lower().split()
+word_count = {}
+
+for word in words:
+    if word not in word_count:
+        word_count[word] = 1
+    else:
+        word_count[word] += 1
+
+print(word_count)
